33e729ae9358edbc298342a253499414e2f79bab
Austin James <austinjames@Austins-MacBook-Air.local>
2022-10-25 21:08:47 -0400

    update

--- index.js
+++ index.js
@@ -13,10 +13,39 @@ content.append(dogContent)
 let dogImage = document.createElement('img')
 dogImage.setAttribute('class', 'dog-image')
 dogImage.setAttribute('src', './assets/rizzo.jpg')
-content.append(dogImage)
+dogContent.append(dogImage)
 
 let dogDetails = document.createElement('div')
 dogDetails.setAttribute('class', 'dog-details')
-content.append(dogDetails)
+dogContent.append(dogDetails)
+
+let titleOne = document.createElement('h3')
+titleOne.append('Description:')
+dogDetails.append(titleOne)
+
+let dogDesc = document.createElement('p') 
+dogDesc.append('This gentle dog is aloof toward her owner, and never comes when called. She always acts as though any stranger she meets will harm her, and dislikes other animals.')
+dogDetails.append(dogDesc)
+
+let titleTwo = document.createElement('h3')
+titleTwo.append("Feeding Times:")
+dogDetails.append(titleTwo)
+
+
+let timeOne = document.createElement('li')
+timeOne.append('9:00 am')
+let timeTwo = document.createElement('li')
+timeTwo.append('12:00 pm')
+let timeThree = document.createElement('li')
+timeThree.append('5:00 pm')
+dogDetails.append(timeOne)
+dogDetails.append(timeTwo)
+dogDetails.append(timeThree)
+
+
+
+
+
+
 
 
